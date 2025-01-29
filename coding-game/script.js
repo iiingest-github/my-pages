11744134889260f4976d5f055db6405312df409a
iiingest-github <76582610+iiingest-github@users.noreply.github.com>
2025-01-29 11:38:27 +0800

    Add files via upload

---
+++ coding-game/script.js
@@ -0,0 +1,598 @@
+﻿var lines = ["0","1",
+"你来到编程补考会场，其他人已经就位了，你向唯一一台空着的电脑走去。",
+"其余三个位置上，分别坐着一个弱智粉毛、憨憨黄毛和傲娇黑长直。",
+"你：<br/>（虽说都到齐了，不过总共也就四个人啊……）",
+"考试铃响，屏幕上出现了本次考试题目。",
+"“当前程序中出现了恶性bug，请将其消除，代码控制在3句以内。”",
+"“所有人都提交后，休息5分钟，回到考场运行程序，运行结果即最终成绩。”",
+"“运行方式：按考号顺序，每人轮流运行一句代码，重复至所有人运行完毕。”",
+"你：<br/>（感觉好复杂的样子。）",
+"你：<br/>（按考号顺序的话，第一个是我，之后是那边的粉毛、黄毛和黑长直。）",
+"你：<br/>（会有什么影响吗……算了不管了。）",
+"你：<br/>（第一句代码写什么呢？）",
+"你：<br/>（第二句代码写什么呢？）",
+"你：<br/>（第三句代码写什么呢？）",
+"你：<br/>嘛，就这样吧。",
+"屏幕上显示所有人都提交完毕，进入休息时间。",
+"你：<br/>（嗯。要和谁说说话看吗？）",
+"你：<br/>午好。",//粉毛
+"粉毛：<br/>嘻嘻嘻……",
+"你：<br/>什么事那么好笑？",
+"粉毛：<br/>世界要完蛋了！",
+"你：<br/>……",
+"你：<br/>我也这么觉得。",//b1
+"粉毛：<br/>诶，干嘛这么消沉？",
+"你：<br/>我答得乱七八糟，感觉已经凉透了。",
+"粉毛：<br/>你的代码有区分大小写吗？",
+"你：<br/>忘了。",
+"粉毛：<br/>那完了呀！",
+"粉毛：<br/>其实……根本不需要区分大小写！哈哈哈哈哈哈哈哈哈……",
+"你：<br/>……谢谢，有被安慰到。",
+"",
+"你：<br/>刚才的题你有思路吗？",//b2
+"粉毛：<br/>全部都destroy掉不就好了嘛！",
+"你：<br/>看来没有。",
+"粉毛：<br/>帽子亲看起来很爱学习的样子呢？",
+"你：<br/>并不。",
+"你：<br/>只是觉得自己太菜了，想求大佬指点一下而已。",
+"粉毛：<br/>嘻嘻嘻，等会考完你还在的话，就给你课后辅导哦！",
+"你：<br/>那是什么方面的辅导啊。",
+"",
+"你：<br/>你知道如何操作对象吗？",//b3
+"粉毛：<br/>啊啊！我会我会！",
+"粉毛：<br/>只要运行X.destroy，所有名为X的对象都会被销毁，真是非常方便的功能呢！",
+"你：<br/>不，听起来很糟糕。",
+"粉毛：<br/>说起来，我应该先用a.rename d和c.rename d，把ac都重命名为d……",
+"粉毛：<br/>最后再用d.destroy，一次销毁三个对象……",
+"粉毛：<br/>想想就觉得很爽呢！哈哈哈哈哈哈！",
+"你：<br/>（……还是不要靠近她比较好。）",
+"",
+"你：<br/>午好。",//黄毛
+"黄毛：<br/>嗯嗯。",
+"你：<br/>刚才的题怎么样？",
+"黄毛：<br/>那当然是做出来了！",
+"你：<br/>（好、好厉害。）",
+"你：<br/>……",
+"你：<br/>那么正确答案是？",//c1
+"黄毛：<br/>第一句是“getbug e”，意思是追踪bug，将其命名为e！",
+"你：<br/>对哦。",
+"黄毛：<br/>第二句是“e.destroy”，意思是令e自我销毁！",
+"你：<br/>嗯嗯。",
+"黄毛：<br/>以上！",
+"你：<br/>第三句呢？",
+"黄毛：<br/>没有惹！",
+"你：<br/>（会这么简单吗。）",
+"",
+"你：<br/>有什么好的学习方法吗？",//c2
+"黄毛：<br/>没有，只是我记忆力超强，把所有的题目都背下来了！",
+"你：<br/>哦哦，厉害厉害。",
+"黄毛：<br/>这次的考点只有5个指令，以及对象的操作方法。",
+"你：<br/>我都不知道唉，是哪5个？",
+"黄毛：<br/>sleep、awake、rename X、destroy和getbug X！",
+"你：<br/>原来如此。",
+"黄毛：<br/>对象的操作就是，当你想操作对象X时，在指令前加上X.就好啦！",
+"你：<br/>你看起来很厉害，为什么补考？",
+"黄毛：<br/>上次考试把输入法弄错成全角了！",
+"你：<br/>（……可能也没有那么厉害吧。）",
+"",
+"你：<br/>您太强了，请嫁给我吧。",//c3
+"黄毛：<br/>好哇！",
+"黄毛：<br/>晚上上床吧！",
+"你：<br/>惊！",
+"黄毛：<br/>啊哈哈哈，开玩笑的。",
+"黄毛：<br/>不过我这里有一些学习资料，可能对你有用，之后给你看看？",
+"你：<br/>奈斯。",
+"",
+"你：<br/>午好。",//黑长直
+"黑长直：<br/>……",
+"你：<br/>那个……",
+"黑长直：<br/>哼。",
+"你：<br/>（唔，很难交流的样子。）",
+"你：<br/>……",
+"你：<br/>做完题感觉如何？",//d1
+"黑长直：<br/>凉了。",
+"你：<br/>我也是。",
+"黑长直：<br/>哼，我跟你不一样。",
+"你：<br/>怎么了？",
+"黑长直：<br/>你知道这次题目最阴险的地方在哪吗？",
+"黑长直：<br/>“每人轮流运行一句代码”，说明我们的代码可能会互相干扰。",
+"你：<br/>这么一说好像是哦。",
+"黑长直：<br/>不知道会演变成什么局面呢……",
+"",
+"你：<br/>大佬，求抱大腿。",//d2
+"黑长直：<br/>滚，我写代码最烂了，每次运行都是满屏叉。",
+"你：<br/>啊这。",
+"黑长直：<br/>小白说我每次都虚构出不存在的对象来。",
+"黑长直：<br/>话说，有bug这个对象吗？",
+"你：<br/>没有吧。",
+"黑长直：<br/>好吧，那我又写错了。",
+"你：<br/>你朋友叫小白？",
+"黑长直：<br/>不行吗？",
+"你：<br/>不不，只是觉得有趣而已。",
+"黑长直：<br/>滚。",
+"你：<br/>？？？",
+"",
+"你：<br/>回见。",//d3
+"黑长直：<br/>哦。",
+"你走了几步，转过头。",
+"黑长直：<br/>……你在做什么？",
+"你：<br/>我想看看你会不会挽留我。",
+"黑长直：<br/>你想多了。",
+"你：<br/>那你为什么要跟上来？",
+"黑长直：<br/>是你非要走在我面前。",
+"你：<br/>……",
+"黑长直：<br/>……咳。",
+"黑长直：<br/>如果你无论如何都想和我做朋友的话，那我就勉……",
+"",
+"铃声响起，你们只好结束对话，回到了座位上。",//休息结束
+"你：<br/>（每人轮流运行一句吗……那首先就是我了。）",
+"你：<br/>运行【",
+"",//判断第一句
+"一阵困意突然向你袭来，你还没来得及思考，就倒在桌上失去了意识。",
+"【BE 2/3：晚安世界】",
+"你感觉身体一空，还没来得及思考，便失去了意识。",
+"【BE 1/3：突然去世】",
+"",
+"粉毛：<br/>运行【a.destroy】。",
+"",
+"黄毛：<br/>运行【getbug e】。",
+"没有发生什么特别的事情。",
+"黑长直：<br/>运行【rename e】。",
+"没有发生什么特别的事情。",
+"黄毛：<br/>姐妹，你的命名跟我撞车了。",
+"黑长直：<br/>我也不懂，我随便写的。",
+"黄毛：<br/>完了！",
+"你：<br/>运行【",
+"",//判断第二句
+"粉毛：<br/>运行【c.destroy】。",
+"",
+"黄毛：<br/>运行【e.destroy】。",
+"",
+"黑长直：<br/>运行【bug.destroy】。",
+"",
+"你：<br/>运行【",
+"",//判断第三句
+"粉毛：<br/>运行【d.destroy】。",
+"",
+"黄毛：<br/>运行【】。",
+"没有发生什么特别的事情。",
+"黑长直：<br/>运行【】。",
+"没有发生什么特别的事情。",
+"程序运行结束了。",
+"",//判断人数和分数
+"屏幕上显示出了你的分数——",
+"",//分数
+"",//进入结局
+"受到如此强烈的视觉冲击，你头上的帽子和假发一下子飞了出去，但都无所谓了。",//100分
+"你：<br/>我　太　强　了　！",
+"黄毛：<br/>我是满分！！！",
+"黑长直：<br/>我也是。",
+"你：<br/>啊这。　",
+"黄毛：<br/>嗯？你是谁？",
+"黑长直：<br/>不认识唉。",
+"你：<br/>……　",
+"泪，射了出来。",
+"趁两人还没反应过来，你狂奔出了教室，从此再也没有回来。",
+"为了改变秃头的命运，你开始发奋图强、刻苦学医。",
+"最终，你成为了一名植发专家。",
+"【NE 3/3：弃技从医】",
+"你：<br/>！！！",//60或70分
+"你：<br/>及格了，奈斯！",
+"你激动地冲出了教室，感觉可以马上吃下三碗饭。",
+"",//判断分数，生存者
+"黄毛：<br/>帽子似乎心情不错呢！",//60分
+"你：<br/>嗯哼。",
+"黄毛：<br/>对了，之前说要给你学习资料，跟我一起回去拿吧！",
+"黑长直：<br/>等等，她之前说想和我出去玩。",
+"你：<br/>有吗？",
+"黑长直：<br/>有。",
+"你：<br/>那……",
+"黄毛：<br/>去我家！",
+"黑长直：<br/>跟我走。",
+"你：<br/>喂喂。",
+"你：<br/>就不能大家一起——",
+"腹部突然一抽筋，大概是被两拳同时击中了。",
+"你缓缓倒下，两人的争吵声逐渐远去。",
+"啊——",
+"也许，这样，也不错呢……",
+"【NE 2/3：修罗场】",
+"黄毛：<br/>帽子！",//黄毛HE
+"你：<br/>？",
+"黄毛：<br/>之前好像说要给你学习资料，要不要看看？",
+"你：<br/>看看吧。",
+"获得了黄毛的学习资料。",
+"你：<br/>……",
+"你：<br/>这好像是℃语言吧。",
+"黄毛：<br/>咦！我们学的不是℃语言吗？",
+"黄毛：<br/>……",
+"你：<br/>……",
+"你：<br/>（啊，里面有张纸条。）",
+"你：<br/>（上面写着“管理员名称：hl369”）。",
+"你：<br/>（是什么意思呢？）",
+"黄毛：<br/>抱歉浪费了你的时间，我会补偿你的……",
+"黄毛：<br/>晚上上床吧！",
+"你：<br/>啊？",
+"从此，你和憨憨黄毛过上了没羞没臊的生活。",
+"【HE 1/3：新婚夫妇】",
+"黑长直：<br/>等下。",//黑长直HE
+"你：<br/>？",
+"黑长直：<br/>……",
+"黑长直：<br/>听说有人使用hack指令打开了次元壁大门，至今还不知道是什么原理。",
+"黑长直：<br/>我也试着hack了一下，但感觉并没有什么卵用。",
+"你：<br/>嗯，和我有什么关系吗？",
+"黑长直：<br/>面对这种都市传说，你肯定十分好奇吧。",
+"你：<br/>有吗？",
+"黑长直：<br/>虽然……我对图书馆毫无兴趣，但如果你无论如何都想去的话，我就勉为其难地陪你去吧！",
+"你：<br/>原来你想去啊。",
+"黑长直：<br/>呵呵，开什么玩笑。",
+"你：<br/>你朋友不陪你去吗？",
+"黑长直：<br/>……",
+"你：<br/>那小白……",
+"黑长直：<br/>滚！",
+"你：<br/>？？？",
+"从此，你和傲娇黑长直过上了幸福又别扭的生活。",
+"【HE 2/3：口嫌体直】",
+"你：<br/>……",//30分
+"你：<br/>我果然还是不适合编程mmp。",
+"怀着沉痛的心情，你离开了教室。",
+"",//判断b是否醒
+"弱智粉毛不知什么时候跟在了你后面。",//粉毛NE
+"粉毛：<br/>帽子亲成绩如何？",
+"你：<br/>凉了。",
+"粉毛：<br/>啊哈哈哈哈哈哈哈哈哈——",
+"你：<br/>滚啦。",
+"粉毛：<br/>对了我之前好像说要给你做辅导来着！",
+"你：<br/>有吗？",
+"粉毛：<br/>队友被destroy掉的话是会扣分的哟！",
+"粉毛：<br/>队友陷入sleep状态也是会扣分的哟！",
+"你：<br/>怎么自己开始解说了啊。",
+"粉毛：<br/>以及bug没有被destroy掉的话就会扣很多分呢！",
+"粉毛：<br/>所以……全部都destroy掉不就好了嘛！哈哈哈！",
+"你：<br/>又来了又来了。",
+"听着粉毛的魔性解说，你度过了身心俱疲的一天。",
+"嗯……准备下一次的补考吧。",
+"【NE 1/3 菜鸡日常】",
+"你：<br/>……",//0分
+"你：<br/>我果然还是不适合编程mmp。",
+"你怀着沉痛的心情独自离开了教室。",
+"",//判断b是否醒
+"你摘下帽子，摸了摸这些年来被编程折磨秃掉的头顶，惆怅地望向远方。",//学医结局
+"你：<br/>不行。　",
+"你：<br/>不能再这样下去了。　",
+"从那以后，你离开了学校，再也没有回来。",
+"",
+"弱智粉毛不知什么时候跟在了你后面。",//粉毛HE
+"粉毛：<br/>帽子亲成绩如何？",
+"你：<br/>凉了。",
+"粉毛：<br/>啊哈哈哈哈哈哈哈哈哈——",
+"你：<br/>滚啦。",
+"粉毛：<br/>那是当然了，因为你完全做错了嘛！和要求完全相反了啊哈哈哈哈哈！！！",
+"你：<br/>果然恶性bug就是你啊。",
+"粉毛：<br/>嘻嘻嘻，原来你发现了吗？",
+"你：<br/>只有你看起来最像吧。",
+"粉毛：<br/>那么作为奖励，告诉你一件不得了的事情吧。",
+"粉毛：<br/>有一次我黑到了管理员密码，是123456！",
+"粉毛：<br/>只要管理员输入这个密码，就能取得更多权限哦！",
+"粉毛：<br/>怎样！是不是炒鸡厉害！",
+"你：<br/>你这样乱黑绝对会被抓起来吧。",
+"粉毛：<br/>哈哈哈哈哈哈——不会的，因为帽子亲会保护我————",
+"你：<br/>才不会啊！",
+"虽然考了0分，但和弱智粉毛的关系似乎变得更好了。",
+"勉强……算是好事吧？",
+"【HE 3/3：恶魔的奴仆】",
+"你：<br/>……",//阴暗面BE
+"你：<br/>哼，也不过如此么。",
+"望着空无一人的教室，你露出了满意的笑容，转身离开。",
+"由于误伤同学赔不起医药费，你逃出了学校，开始了恶霸生活。",
+"最终由于抢人老婆，被消毒大队打死在儒家酒店。",
+"也许……这就是现实吧。",
+"【BE 3/3 阴暗面】",
+"四周传来巨响，你还没反应过来，便失去了意识。",//隐藏结局
+"当你醒来时，发现自己正处于一个虚无的空间内。",
+"你随便走了一会儿，看到了一行字：",
+"“感谢你费尽心思做到这一步，但我们已经修复了这个bug，你不能再去往其他世界，只能永远留在这里。”",
+"“为了让你不那么无聊，我们为你画了一幅画，你可以看着它打发时间。”",
+"<img src='thanks.jpg' style='flex-basis:400px;height:400px;'>",
+"你：<br/>……",
+"你：<br/>妈耶！",
+"从此，你在虚无空间中过上了无聊的看画生活。",
+"【SE 1/1：最高编程境界】"
+];
+var slines = [
+"没有发生什么特别的事情。",
+"突然倒在了桌上，看样子是睡着了。",
+"从教室中消失了。",
+"醒了过来。"
+];
+var score = 100;
+var i = 1;//初始为1
+var code = ["","",""];//初始为空
+var cmds = [["","",""],["","",""],["","",""]];
+var states = [["a",0,0,""],["b",0,0,"粉毛"],["c",0,0,"黄毛"],["d",0,0,"黑长直"]];//id、是否死亡、是否睡眠、姓名
+var hack = false;
+
+function next(){
+	if(document.getElementById("next").className.match("disable")==null){
+		i += 1;
+		predo();
+		var newdiv = document.createElement("div");
+		newdiv.className = "line";
+		if(lines[i].match("你：")){
+			if(lines[i].match("　")){charaline("a2.png",newdiv);}
+			else{charaline("a.png",newdiv);}}
+		else if(lines[i].match("粉毛：")){
+			charaline("b.png",newdiv);}
+		else if(lines[i].match("黄毛：")){
+			charaline("c.png",newdiv);}
+		else if(lines[i].match("黑长直：")){
+			charaline("d.png",newdiv);}
+		else{
+			newdiv.innerHTML = lines[i];}
+		document.getElementById("content").appendChild(newdiv);
+		check();
+		window.scrollTo(0,document.body.scrollHeight);
+	}
+}
+function predo(){
+	switch(i){
+		case 31:case 40:case 49:case 65:case 77:case 85:case 101:case 114:case 126:i = 127;break;
+		case 128:document.getElementById("loadall").className += " disabled";break;
+		case 129:lines[i] += (code[0] + "】。");break;
+		case 130:transcode(0);cmd1();break;
+		case 136:if(states[1][1]+states[1][2] > 0){i = 138;}break;//粉毛状态1
+		case 137:if(states[0][0]=="a"){i = 133;}else{lines[i] = slines[0];}break;//粉毛代码1
+		case 145:lines[i] += (code[1] + "】。");break;
+		case 146:transcode(1);cmd2();break;
+		case 147:if(states[1][1]+states[1][2] > 0){i = 149;}else{break;}//粉毛状态2
+		case 149:if(states[2][1]+states[2][2] > 0){i = 151;}else{break;}//黄毛状态2
+		case 151:if(states[3][1]+states[3][2] > 0){i = 153;}else{break;}//黑长直状态2
+		case 153:lines[i] += (code[2] + "】。");break;
+		case 148:kill("c");break;//粉毛代码2
+		case 150:kill("e");break;//黄毛代码
+		case 152:kill("bug");break;//黑长直代码
+		case 154:transcode(2);cmd3();break;
+		case 155:if(states[1][1]+states[1][2] > 0){i = 157;}else{break;}//粉毛状态3
+		case 157:if(states[2][1]+states[2][2] > 0){i = 159;}else{break;}//黄毛状态3
+		case 159:if(states[3][1]+states[3][2] > 0){i = 161;}else{break;}//黑长直状态3
+		case 161:document.getElementById("loadall").className = "subbtn";break;
+		case 156:kill("d");break;//粉毛代码3
+		case 162:checknum();break;
+		case 164:lines[i] = score + "。";break;
+		case 165:finalcheck();break;
+		case 182:if(score==70){if(states[2][1]+states[2][2]==0){i=199;}else{i=217;}}else{i+=1;}break;
+		case 238:if(states[1][2]){if(states[2][1]+states[2][2]==0){i=199;}else{i=217;}}else{i+=1;}break;
+		case 258:if(states[1][2]){i+=1;}else{i=264;}break;
+		case 263:i = 176;break;
+	}
+}
+function check(){
+	switch(i){
+		case 12:createinp("inp1");break;
+		case 13:code[0] = document.getElementById("inp1").value;
+			document.getElementById("inp1").setAttribute("disabled",true);
+			createinp("inp2");break;
+		case 14:code[1] = document.getElementById("inp2").value;
+			document.getElementById("inp2").setAttribute("disabled",true);
+			createinp("inp3");break;
+		case 15:code[2] = document.getElementById("inp3").value;
+			document.getElementById("inp3").setAttribute("disabled",true);
+			document.getElementById("loadall").removeAttribute("hidden");
+			document.getElementById("next").removeAttribute("hidden");
+			document.getElementById("submit").setAttribute("hidden",true);break;
+		case 17:showbtn("弱智粉毛","憨憨黄毛","傲娇黑长直",18,50,86);break;
+		case 22:showbtn("我也觉得","刚才的题","请教代码",23,32,41);break;
+		case 55:showbtn("正确答案","学习方法","请嫁给我",56,66,78);break;
+		case 91:showbtn("感觉如何","大佬大佬","回见",92,102,115);break;
+		case 132:case 134:case 178:case 198:case 216:case 234:case 254:case 282:case 289:case 299:end();break;
+	}
+}
+
+function charaline(src,newdiv){
+	var newimg = document.createElement("img");
+	newimg.src = src;
+	var newdiv2 = document.createElement("div");
+	newdiv2.className = "lline";
+	newdiv2.innerHTML = lines[i];
+	newdiv.appendChild(newimg);
+	newdiv.appendChild(newdiv2);
+}
+function createinp(id){
+	var newdiv = document.createElement("div");
+	newdiv.className = "line";
+	var newinp = document.createElement("input");
+	newinp.type = "text";
+	newinp.maxlength = "20";
+	newinp.id = id;
+	newdiv.appendChild(newinp);
+	document.getElementById("content").appendChild(newdiv);
+	document.getElementById("loadall").setAttribute("hidden",true);
+	document.getElementById("next").setAttribute("hidden",true);
+	document.getElementById("submit").removeAttribute("hidden");
+}
+function showbtn(n1,n2,n3,i1,i2,i3){
+	var newdiv = document.createElement("div");
+	newdiv.className = "lbtns";
+	var btn1 = document.createElement("button");
+	btn1.innerHTML = n1;
+	btn1.id = i1;
+	btn1.className = "lbtn";
+	btn1.setAttribute("onclick","clickbtn(this);");
+	newdiv.appendChild(btn1);
+	var btn2 = document.createElement("button");
+	btn2.innerHTML = n2;
+	btn2.id = i2;
+	btn2.className = "lbtn";
+	btn2.setAttribute("onclick","clickbtn(this);");
+	newdiv.appendChild(btn2);
+	var btn3 = document.createElement("button");
+	btn3.innerHTML = n3;
+	btn3.id = i3;
+	btn3.className = "lbtn";
+	btn3.setAttribute("onclick","clickbtn(this);");
+	newdiv.appendChild(btn3);
+	document.getElementById("content").appendChild(newdiv);
+	document.getElementById("loadall").className += " disabled";
+	document.getElementById("next").className += " disabled";
+}
+function clickbtn(me){
+	var lbtns = document.getElementsByClassName("lbtns")[0];
+	lbtns.setAttribute("hidden",true);
+	i = parseInt(me.id) - 1;
+	document.getElementById("loadall").className = "subbtn";
+	document.getElementById("next").className = "mainbtn";
+	next();
+	document.getElementById("content").removeChild(lbtns);
+}
+function end(){
+	document.getElementById("loadall").setAttribute("hidden",true);
+	document.getElementById("next").setAttribute("hidden",true);
+	document.getElementById("replay").removeAttribute("hidden");
+}
+function skip(){
+	var lbtn = document.getElementById("loadall");
+	while((lbtn.hidden==false)&&(lbtn.className!="subbtn disabled")){
+		next();
+	}
+}
+
+function transcode(index){
+	var lcode = code[index].toLowerCase();
+	var cmd = ["","",""];
+	switch(lcode.split(" ").length){
+		case 1:cmd[1] = lcode;break;
+		case 2:cmd[1] = lcode.split(" ")[0];cmd[2] = lcode.split(" ")[1];break;
+		default:return;}
+	switch(cmd[1].split(".").length){
+		case 1:break;
+		case 2:cmd[0] = cmd[1].split(".")[0];cmd[1] = cmd[1].split(".")[1];break;
+		default:return;}
+	switch(cmd[1]){
+		case "hack":case "123456":if((cmd[0]!="")||(cmd[2]!="")){return;}break;
+		case "sleep":case "awake":case "destroy":if(cmd[2]!=""){return;}break;
+		case "rename":case "getbug":if(cmd[2]==""){return;}break;
+		default:return;}
+	cmds[index] = cmd;//alert(cmds[index]);
+}
+function cmd1(){
+	var cmd = cmds[0];
+	switch(cmd[1]){
+		case "sleep":
+			if((cmd[0]=="")||(cmd[0]==states[0][0])){i = 131;return;}
+			else if(cmd[0]=="b"){i = 135;lines[i] = "粉毛" + slines[1];states[1][2] = 1;return;}
+			else if(cmd[0]=="c"){i = 135;lines[i] = "黄毛" + slines[1];;states[2][2] = 1;return;}
+			else if(cmd[0]=="d"){i = 135;lines[i] = "黑长直" + slines[1];;states[3][2] = 1;return;}break;
+		case "destroy":
+			if((cmd[0]=="")||(cmd[0]==states[0][0])){i = 133;return;}
+			else if(cmd[0]=="b"){i = 135;lines[i] = "粉毛" + slines[2];states[1][1] = 1;return;}
+			else if(cmd[0]=="c"){i = 135;lines[i] = "黄毛" + slines[2];states[2][1] = 1;return;}
+			else if(cmd[0]=="d"){i = 135;lines[i] = "黑长直" + slines[2];states[3][1] = 1;return;}break;
+		case "rename":
+			for(var j=0;j<4;j++){
+				if(cmd[0]==""){states[0][0]=cmd[2];}
+				else if(cmd[0]==states[j][0]){states[j][0]=cmd[2];}
+			}
+	}
+	i = 135;lines[i] = slines[0];//alert(states);
+}
+function cmd2(){
+	var cmd = cmds[1];
+	states[1][0] = "e";
+	states[3][0] = "e";
+	switch(cmd[1]){
+		case "123456":hack = true;break;
+		case "sleep":
+			if((cmd[0]=="")||(cmd[0]==states[0][0])){i = 131;return;}
+			else if((cmd[0]=="c")&&(states[2][1]+states[2][2]==0)){lines[i] = "黄毛" + slines[1];states[2][2] = 1;return;}
+			else if(cmd[0]=="e"){
+				if(states[1][1]+states[1][2]==0){lines[i] = "粉毛、黑长直" + slines[1];states[1][2] = 1;states[3][2] = 1;return;}
+				else{lines[i] = "黑长直" + slines[1];states[3][2] = 1;return;}
+			}break;
+		case "destroy":
+			if((cmd[0]=="")||(cmd[0]==states[0][0])){i = 133;return;}
+			else if((cmd[0]=="c")&&(states[2][1]==0)){lines[i] = "黄毛" + slines[2];states[2][1] = 1;return;}
+			else if(cmd[0]=="e"){
+				if(states[1][1]==0){lines[i] = "粉毛、黑长直" + slines[2];states[1][1] = 1;states[3][1] = 1;return;}
+				else{lines[i] = "黑长直" + slines[2];states[3][1] = 1;return;}
+			}break;
+		case "awake":
+			if((cmd[0]=="e")&&(states[1][1]==0)&&(states[1][2]==1)){lines[i] = "粉毛" + slines[3];states[1][2] = 0;return;}
+		case "rename":
+			for(var j=0;j<4;j++){
+				if(cmd[0]==""){states[0][0]=cmd[2];}
+				else if(cmd[0]==states[j][0]){states[j][0]=cmd[2];}}break;
+		case "getbug":states[1][0] = cmd[2];
+	}
+	lines[i] = slines[0];//alert(states);
+}
+function kill(str){
+	if(states[0][0]==str){i = 133;}
+	else{
+		var tmp = "";
+		for(var j=1;j<4;j++){
+			if((states[j][0]==str)&&(states[j][1]==0)){states[j][1]=1;tmp+=("、"+states[j][3]);}
+		}
+		if(tmp==""){lines[i] = slines[0];}
+		else{lines[i] = tmp.substr(1)+slines[2];}
+	}
+}
+function cmd3(){
+	var cmd = cmds[2];
+	switch(cmd[1]){
+		case "hack":
+			if((hack==true)&&(states[0][0]=="hl369")){i = 290;return;}break;
+		case "sleep":
+			if((cmd[0]=="")||(cmd[0]==states[0][0])){i = 131;return;}
+			else{
+				var tmp = "";
+				for(var j=1;j<4;j++){
+					if((states[j][0]==cmd[0])&&(states[j][1]+states[j][2]==0)){states[j][2]=1;tmp+=("、"+states[j][3]);}}
+				if(tmp!=""){lines[i] = tmp.substr(1)+slines[1];return;}}
+			break;
+		case "destroy":
+			if((cmd[0]=="")||(cmd[0]==states[0][0])){i = 133;return;}
+			else{
+				var tmp = "";
+				for(var j=1;j<4;j++){
+					if((states[j][0]==cmd[0])&&(states[j][1]==0)){states[j][1]=1;tmp+=("、"+states[j][3]);}}
+				if(tmp!=""){lines[i] = tmp.substr(1)+slines[2];return;}}
+			break;
+		case "awake":
+			if((states[1][0]==cmd[0])&&(states[1][1]==0)&&(states[1][2]==1)){lines[i] = "粉毛" + slines[3];states[1][2] = 0;return;}
+			else if((states[2][0]==cmd[0])&&(states[2][1]==0)&&(states[2][2]==1)){lines[i] = "黄毛" + slines[3];states[2][2] = 0;return;}break;
+		case "rename":
+			for(var j=0;j<4;j++){
+				if(cmd[0]==""){states[0][0]=cmd[2];}
+				else if(cmd[0]==states[j][0]){states[j][0]=cmd[2];}}break;
+		case "getbug":states[1][0] = cmd[2];
+	}
+	lines[i] = slines[0];//alert(states);
+}
+
+function checknum(){
+	var dead = "";
+	var sleep = "";
+	if(states[1][1]==1){dead += "、"+states[1][3];}
+	else{
+		score -= 40;if(states[1][2]==1){sleep += "、"+states[1][3];}}
+	if(states[2][1]==1){dead += "、"+states[2][3];score -= 30;}
+	else{
+		if(states[2][2]==1){sleep += "、"+states[2][3];score -= 30;}}
+	if(states[3][1]==1){dead += "、"+states[3][3];score -= 30;}
+	else{
+		if(states[3][2]==1){sleep += "、"+states[3][3];score -= 30;}}
+	if((dead=="")&&(sleep=="")){lines[i] = "你环顾四周，所有人都紧张地盯着自己的屏幕。";}
+	else{
+		if(dead){dead = "，" + dead.substr(1) + "已经不在了";}
+		if(sleep){sleep = "，" + sleep.substr(1) + "正趴在桌上睡觉";}
+		lines[i] = "你环顾四周" + dead + sleep + "。";
+	}
+}
+function finalcheck(){
+	switch(score){
+		case 100:i = 166;break;
+		case 60:case 70:i=179;break;
+		case 30:i = 235;break;
+		case 0:i = 255;break;
+		case 40:if(states[1][1]+states[2][1]+states[3][1]==3){i = 283;}else{states[1][2]=1;i = 255;}break;
+	}
+}
